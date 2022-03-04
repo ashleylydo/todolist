793f4db572b65771ca639cd07d6a40c2c1e4cb0a
Ashley Quyen Ly-Do <87556015+ashleylydo@users.noreply.github.com>
2022-03-04 17:19:33 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,100 @@
-import React, {useState} from "react";
-import "../App.css";
+import {useState} from "react";
+import Add from "./components/Add";
+import Edit from "./components/Edit";
+import DisplayItems from "./components/DisplayItems";
+import "./App.css";
 
-export default function Add({handleAddTask, handleCancelBtn}) {
-    const [newTask, setNewTask] = useState("");
+export default function Home() {
+    const [taskList, setTaskList] = useState(["TodoList Task"]);
+    const [chooseEdit, setChooseEdit] = useState(false);
+    const [chooseAdd, setChooseAdd] = useState(false);
+    const [chosenTask, setChosenTask] = useState({id: -1, name: ""});
 
-    function addTask(e) {
-        handleAddTask(e, newTask);
+    function handleCancelBtn() {
+        setChooseEdit(false);
+        setChooseAdd(false);
+    }
+
+    function handleAddTask(e, newTask) {
+        e.preventDefault();
+
+        if (newTask !== "") {
+            setTaskList([...taskList, newTask]);
+            handleCancelBtn();
+        }
+    }
+
+    function handleDeleteTask(name) {
+        const removeItem = taskList.filter((task) => {
+            return task !== name;
+        });
+        setTaskList(removeItem);
+    }
+
+    function handleEditBtn() {
+        setChooseEdit(true);
+    }
+
+    function handleEditTask(index, name) {
+        setChosenTask(chosenTask => ({...chosenTask, ...{id: index, name: name}}))
+    }
+
+    function handleChanges(id, name) {
+        let tempArr = [...taskList];
+        tempArr[id] = name;
+        setTaskList(tempArr);
+    }
+
+    if (chooseAdd) {
+        return (
+            <div>
+                <Add
+                    handleAddTask={handleAddTask}
+                    handleCancelBtn={handleCancelBtn}
+                />
+            </div>
+        );
+    }
+
+    if (chooseEdit) {
+        return (
+            <div>
+                <Edit
+                    chosenTask={chosenTask}
+                    handleCancelBtn={handleCancelBtn}
+                    handleChanges={handleChanges}
+                />
+            </div>
+        );
     }
 
     return (
-        <form onSubmit={addTask}>
-            <h1>Add New Task</h1>
-            <label>Name</label>
+        <div>
             <br/>
-            <input
-                name="tempTask"
-                type="text"
-                value={newTask}
-                placeholder="Please enter the task name"
-                onChange={(e) => setNewTask(e.target.value)}
-            />
             <br/>
-            <button className="btn-form" type="submit">Add</button>
-            <button className="btn-form  btn-cancel" type="button" onClick={handleCancelBtn}>Cancel</button>
-        </form>
+            <button className = "btn-home" onClick={() => setChooseAdd(true)}>Add Task</button>
+            <h2>List Task</h2>
+            <table>
+                <thead>
+                <tr>
+                    <th>Name of task</th>
+                    <th>Edit Task</th>
+                    <th>Delete Task</th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                    taskList.map((name, index) => (
+                        <DisplayItems
+                            name={name}
+                            handleEditBtn={handleEditBtn}
+                            handleDeleteTask={handleDeleteTask}
+                            index={index}
+                            handleEditTask={handleEditTask}
+                        />
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 }
